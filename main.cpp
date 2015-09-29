9f15cd2810e59cd25e70d2e0517dff5a9777edbc
Peter Bailey <peter.eldridge.bailey@gmail.com>
2015-09-29 16:46:10 -0700

    add note

--- main.cpp
+++ main.cpp
@@ -182,6 +182,7 @@ int main (int argc, char** argv)
 			needrefresh = false;
 		}
 
+		//!@todo no periodic sleeping! Use a wakeup event.
 		// If no packets were read at all this iteration, pause to prevent 100%
 		// CPU utilisation;
 		if (!packets_read)
